a592729db90eced29158bf63633f1f1260681dda
wasim <wasim@agilecodex.com>
2021-04-29 17:30:09 +0600

    Agilecodex Backend version 1.3.5

--- Model/System/Config/Backend/SaveConfig.php
+++ Model/System/Config/Backend/SaveConfig.php
@@ -15,23 +15,26 @@ class SaveConfig extends \Magento\Framework\App\Config\Value
     private $writerInterface;
     
     /**
+     * 
      * @param \Magento\Framework\Model\Context $context
      * @param \Magento\Framework\Registry $registry
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
      * @param \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
+     * @param WriterInterface $writerInterface
      * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
      * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
      * @param array $data
      */
+    
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\App\Config\ScopeConfigInterface $config,
         \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        WriterInterface $writerInterface,    
         \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = [],
-        WriterInterface $writerInterface    
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,        
+        array $data = []           
     ) {
         $this->writerInterface = $writerInterface;
         parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
@@ -48,7 +51,6 @@ class SaveConfig extends \Magento\Framework\App\Config\Value
         
         $value = $this->getValue();
         $path = $this->getPath();
-        $result = [];
         
         if($path == self::CSP_FRONTEND_URI){
             $this->writerInterface->save(self::CSP_FRONTEND_URI, $value);

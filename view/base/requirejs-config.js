e48a6aee2920c706342e36e36c6b827218950f7d
Wasim Haider Chowdhury <wasimhc@gmail.com>
2024-02-16 11:29:25 +0600

    ACX-318 & OM-49: untracked 2.4.4 & 2.4.6 compatibility
    Merged untracked 2.4.4 & 2.4.6 compatibility files.

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ *  Copyright © Agile Codex Ltd. All rights reserved.
+ *  License: https://www.agilecodex.com/license-agreement
+ */
+
+var config = {
+    paths: {
+        'Acx_Backend/js/owl.carousel.min': 'Acx_Backend/js/owl.carousel.min',
+        acxIonRangeSlider: 'Acx_Backend/js/ion.rangeSlider.min',
+    },
+    shim: {
+        "Acx_Backend/js/owl.carousel.min": ["jquery"],
+        acxIonRangeSlider: ["jquery"],
+    }
+};
